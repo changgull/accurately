5ed85baafc9009ab2b92645b10f372f20f58e0bf
Changgull Song <changgull@gmail.com>
2021-06-27 09:43:29 -0700

    Initial Commit

---
+++ Accurately/AccuratelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccuratelyApp.swift
+//  Accurately
+//
+//  Created by Chang Song on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AccuratelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accurately/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accurately
+//
+//  Created by Chang Song on 6/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
